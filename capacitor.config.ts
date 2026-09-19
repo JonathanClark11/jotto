9041c841361a8b77817f28cb7ac4d848edd56e43
Bucky Dunston <buckydunston@gmail.com>
2026-09-19 14:01:42 -0700

    fix: rename bundle ID and app display name from Jotto to Cinq

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.jonathanclark11.jotto",
+  appId: "com.jonathanclark11.cinq",
   appName: "Cinq",
   webDir: "dist/client",
   server: {
